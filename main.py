e189c5b0ca749d5259bcaceaf88ac7eb2aafb02f
Danilo Lemes <daniloleemes@hotmail.com>
2017-10-28 13:48:02 -0200

    add bsb ceps

--- main.py
+++ main.py
@@ -5,35 +5,28 @@ import json
 import re
 from lxml import etree
 
-url = "http://www.brasilao.com/cep/df/"
-hxs = lxml.html.document_fromstring(requests.get(url).content)
-# pagina final 1000
-# hxs = lxml.html.document_fromstring(requests.get("http://buscaceps.com.br/estado/goias/pagina/").content)
+hxs = lxml.html.document_fromstring(requests.get("http://buscaceps.com.br/cidade/distrito-federal/brasilia/pagina/").content)
 # pagina final 48087
-
-root_container = '//span[@class="codigos-postais2-container"]'
-cep_container = ".//span[@class='codigo-postais2-title']/b/text()"
-nome_container = ".//a/text()"
 
 with io.open('ceps_bsb.json', 'a', encoding='utf8') as file:
     try:
-        for node in hxs.xpath(root_container):
+        for node in hxs.xpath('//div[@class="post_list"]/div[contains(@class, "item")]'):
             bairro = {}
-            cep = ''.join(re.findall('\d+', node.xpath(cep_container)[0]))
-            nome = node.xpath(nome_container)[0]
+            cep = ''.join(re.findall('\d+', node.xpath(".//a/text()")[0]))
+            nome = node.xpath(".//h3/text()")[0]
             bairro['cep'] = cep
             bairro['nome'] = nome
             bairro['pagina'] = 0
             json.dump(bairro, file, ensure_ascii=False)
             file.write(",\n")
 
-        for page in range(2, 1000):
-            hxs = lxml.html.document_fromstring(requests.get(url + str(page) + ".html").content)
+        for page in range(13, 34515, 13):
+            hxs = lxml.html.document_fromstring(requests.get("http://buscaceps.com.br/cidade/distrito-federal/brasilia/pagina/" + str(page)).content)
             try:
-                for node in hxs.xpath(root_container):
+                for node in hxs.xpath('//div[@class="post_list"]/div[contains(@class, "item")]'):
                     bairro = {}
-                    cep = ''.join(re.findall('\d+', node.xpath(cep_container)[0]))
-                    nome = node.xpath(nome_container)[0]
+                    cep = ''.join(re.findall('\d+', node.xpath(".//a/text()")[0]))
+                    nome = node.xpath(".//h3/text()")[0]
                     bairro['cep'] = cep
                     bairro['nome'] = nome
                     bairro['pagina'] = page
